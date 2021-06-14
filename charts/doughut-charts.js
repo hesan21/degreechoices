23239b5da6174be1fd2dd7728c8e8238c1201e50
Hesan Naveed <hesannaveed21@gmail.com>
2021-06-14 11:01:55 +0500

    Diversity Section Added

--- charts/doughut-charts.js
+++ charts/doughut-charts.js
@@ -1,5 +1,7 @@
 let canvas = document.getElementById("top5-top-scoring");
 let ctx = canvas.getContext("2d")
+let canvas2 = document.getElementById("top5-lowest-scoring");
+let ctx2 = canvas2.getContext("2d")
 
 let top5_top = new Chart(ctx, {
     type: 'doughnut',
@@ -7,14 +9,14 @@ let top5_top = new Chart(ctx, {
         // labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
         datasets: [{
             label: 'Scores',
-            data: [72.3, 27.7],
+            data: [91.26, 8.74],
             backgroundColor: [
                 'rgba(245, 81, 81, 1)',
-                'rgba(222,222,224, 1)',
+                '#999',
             ],
             borderColor: [
                 'rgba(245, 81, 81, 1)',
-                'rgba(222,222,224, 1)',
+                '#999',
             ],
             borderWidth: 1,
             borderRadius: 5
@@ -32,7 +34,7 @@ let top5_top = new Chart(ctx, {
     plugins: [
         {
             afterDraw: function (chart, option) {
-                let theCenterText = "50%" ;
+                let theCenterText = "91.26" ;
                 const canvasBounds = canvas.getBoundingClientRect();
                 const fontSz = Math.floor( canvasBounds.height * 0.15 ) ;
                 chart.ctx.textBaseline = 'middle';
@@ -44,26 +46,20 @@ let top5_top = new Chart(ctx, {
         }
     ]
 });
-
-
-
-let canvas2 = document.getElementById("top5-lowest-scoring");
-let ctx2 = canvas2.getContext("2d")
-
 let top5_lowest = new Chart(ctx2, {
     type: 'doughnut',
     data: {
         // labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
         datasets: [{
             label: 'Scores',
-            data: [82.3, 17.7],
+            data: [52.69, 47.31],
             backgroundColor: [
                 'rgba(242, 172, 87, 1)',
-                'rgba(222,222,224, 1)',
+                '#999',
             ],
             borderColor: [
                 'rgba(245, 81, 81, 1)',
-                'rgba(242, 172, 87, 1)',
+                '#999',
             ],
             borderWidth: 1,
             borderRadius:5
@@ -81,8 +77,102 @@ let top5_lowest = new Chart(ctx2, {
     plugins: [
         {
             afterDraw: function (chart, option) {
-                let theCenterText = "82%" ;
-                const canvasBounds = canvas.getBoundingClientRect();
+                let theCenterText = "52.69" ;
+                const canvasBounds = canvas2.getBoundingClientRect();
+                const fontSz = Math.floor( canvasBounds.height * 0.15 ) ;
+                chart.ctx.textBaseline = 'middle';
+                chart.ctx.textAlign = 'center';
+                chart.ctx.font = fontSz+'px Arial';
+                chart.ctx.fillStyle= "white"
+                chart.ctx.fillText(theCenterText, canvasBounds.width/2, canvasBounds.height*0.55 )
+            }
+        }
+    ]
+});
+
+// Diversity
+let canvas3 = document.getElementById("diversity-top-scoring");
+let ctx3 = canvas3.getContext("2d")
+let canvas4 = document.getElementById("diversity-lowest-scoring");
+let ctx4 = canvas4.getContext("2d")
+
+new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        // labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+        datasets: [{
+            label: 'Scores',
+            data: [58.59, 41.41],
+            backgroundColor: [
+                'rgba(245, 81, 81, 1)',
+                '#999',
+            ],
+            borderColor: [
+                'rgba(245, 81, 81, 1)',
+                '#999',
+            ],
+            borderWidth: 1,
+            borderRadius: 5
+        }]
+    },
+    options: {
+        cutout: 110,
+        responsive: true,
+        plugins: {
+            tooltip: {
+                enabled: false
+            }
+        }
+    },
+    plugins: [
+        {
+            afterDraw: function (chart, option) {
+                let theCenterText = "58.59" ;
+                const canvasBounds = canvas3.getBoundingClientRect();
+                const fontSz = Math.floor( canvasBounds.height * 0.15 ) ;
+                chart.ctx.textBaseline = 'middle';
+                chart.ctx.textAlign = 'center';
+                chart.ctx.font = fontSz+'px Arial';
+                chart.ctx.fillStyle= "white"
+                chart.ctx.fillText(theCenterText, canvasBounds.width/2, canvasBounds.height*0.55 )
+            }
+        }
+    ]
+});
+
+new Chart(ctx4, {
+    type: 'doughnut',
+    data: {
+        // labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
+        datasets: [{
+            label: 'Scores',
+            data: [10.81, 89.29],
+            backgroundColor: [
+                'rgba(242, 172, 87, 1)',
+                '#999',
+            ],
+            borderColor: [
+                'rgba(242, 172, 87, 1)',
+                '#999',
+            ],
+            borderWidth: 1,
+            borderRadius: 5
+        }]
+    },
+    options: {
+        cutout: 110,
+        responsive: true,
+        plugins: {
+            tooltip: {
+                enabled: false
+            }
+        }
+    },
+    plugins: [
+        {
+            afterDraw: function (chart, option) {
+                let theCenterText = "10.81" ;
+                const canvasBounds = canvas4.getBoundingClientRect();
                 const fontSz = Math.floor( canvasBounds.height * 0.15 ) ;
                 chart.ctx.textBaseline = 'middle';
                 chart.ctx.textAlign = 'center';
